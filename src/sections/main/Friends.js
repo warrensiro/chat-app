e1cf3f17db189cb4c1452e452b45b18beabcee8d
Warren Siro <warrensiro@gmail.com>
2026-01-01 20:53:15 +0300

    added friends dialog box

---
+++ src/sections/main/Friends.js
@@ -0,0 +1,32 @@
+import { Dialog, Stack, Tab, Tabs } from "@mui/material";
+import React, { useState } from "react";
+
+const Friends = ({ open, handleClose }) => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Dialog
+      fullWidth
+      maxWidth="xs"
+      open={open}
+      keepMounted
+      onClose={handleClose}
+      sx={{ p: 4 }}
+    >
+      <Stack p={2} sx={{ width: "100%" }}>
+        <Tabs value={value} onChange={handleChange} centered>
+          <Tab label="Explore" />
+          <Tab label="Friends" />
+          <Tab label="Requests" />
+        </Tabs>
+      </Stack>
+      {/* dialog content */}
+    </Dialog>
+  );
+};
+
+export default Friends;
