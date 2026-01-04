4226afd2964550731fd63f9159915d68f384db6c
Warren Siro <warrensiro@gmail.com>
2026-01-04 23:58:47 +0300

    socket connection

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,15 +1,58 @@
 import { Stack } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
-import { useSelector } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
+import { connectSocket, socket } from "../../socket";
+import { showSnackbar } from "../../redux/Slices/app";
 
 const DashboardLayout = () => {
+  const dispatch = useDispatch();
+
   const { isLoggedIn } = useSelector((state) => state.auth);
+
+  const user_id = window.localStorage.getItem("user_id");
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      window.onload = function () {
+        if (!window.location.hash) {
+          window.location = window.location + "#loaded";
+          window.location.reload();
+        }
+      };
+
+      window.reload();
+
+      if (!socket) {
+        connectSocket(user_id);
+      }
+
+      // new friend request
+      socket.on("new_friend_request", (data) => {
+        dispatch(showSnackbar({ severity: "success", message: data.message }));
+      });
+
+      socket.on("request_accepted", (data) => {
+        dispatch(showSnackbar({ severity: "success", message: data.message }));
+      });
+
+      socket.on("request_sent", (data) => {
+        dispatch(showSnackbar({ severity: "success", message: data.message }));
+      });
+    }
+
+    return () => {
+      socket.off("new_friend_request")
+      socket.off("request_accepted")
+      socket.off("request_sent")
+    }
+  }, [isLoggedIn, socket]);
+
   if (!isLoggedIn) {
     return <Navigate to="/auth/login" replace />;
   }
+
   return (
     <Stack direction="row">
       {/* sidebar */}
