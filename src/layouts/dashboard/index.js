c93805e8b11f36cdc16ce3e8db28702a3e678eec
Warren Siro <warrensiro@gmail.com>
2026-01-06 20:50:01 +0300

    reducers for conversations

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -4,12 +4,15 @@ import { Navigate, Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import { useDispatch, useSelector } from "react-redux";
 import { connectSocket, socket } from "../../socket";
-import { showSnackbar } from "../../redux/Slices/app";
+import { SelectConversation, showSnackbar } from "../../redux/Slices/app";
+import { AddDirectConversation, UpdateDirectConversation } from "../../redux/Slices/conversation";
 
 const DashboardLayout = () => {
   const dispatch = useDispatch();
 
   const { isLoggedIn } = useSelector((state) => state.auth);
+
+  const { conversations } = useSelector((state) => state.conversation.direct_chat)
 
   const user_id = window.localStorage.getItem("user_id");
 
@@ -40,12 +43,27 @@ const DashboardLayout = () => {
       socket.on("request_sent", (data) => {
         dispatch(showSnackbar({ severity: "success", message: data.message }));
       });
+
+      socket.on("start_chat", (data) => {
+        console.log(data)
+        const existing_conversation = conversations.find((el) => el.id === data._id)
+
+        if (existing_conversation) {
+          // after evaluating to true, get the convo
+          dispatch(UpdateDirectConversation({ conversattion: data }))
+        }
+        else {
+          dispatch(AddDirectConversation({ conversation: data }))
+        }
+        dispatch(SelectConversation({ room_id: data._id}))
+      })
     }
 
     return () => {
-      socket.off("new_friend_request")
-      socket.off("request_accepted")
-      socket.off("request_sent")
+      socket?.off("new_friend_request")
+      socket?.off("request_accepted")
+      socket?.off("request_sent")
+      socket?.off("start_chat")
     }
   }, [isLoggedIn, socket]);
 
--- src/redux/Slices/conversation.js
+++ src/redux/Slices/conversation.js
@@ -35,6 +35,50 @@ const slice = createSlice({
       });
       state.direct_chat.conversations = list;
     },
+
+    updateDirectConversation(state, action) {
+      const this_conversation = action.payload.conversations;
+      state.direct_chat.conversations = state.direct_chat.conversations.map(
+        (el) => {
+          if (el.id !== this_conversation._id) {
+            return el;
+          } else {
+            const user = this_conversation.participants.find(
+              (el) => el._id.toString() !== user_id
+            );
+            return {
+              id: this_conversation._id,
+              user_id: user._id,
+              name: `${this_user.firstName} ${this_user.lastName}`,
+              img: faker.image.avatar(),
+              online: this_user.status === "Online",
+              msg: faker.music.songName(),
+              time: "10:01",
+              unread: 0,
+              pinned: false,
+            };
+          }
+        }
+      );
+    },
+
+    addDirectConversation(state, action) {
+      const this_conversation = action.payload.conversations;
+      const user = this_conversation.participants.find(
+        (el) => el._id.toString() !== user_id
+      );
+      state.direct_chat.conversations.push({
+        id: this_conversation._id,
+        user_id: user._id,
+        name: `${this_user.firstName} ${this_user.lastName}`,
+        img: faker.image.avatar(),
+        online: this_user.status === "Online",
+        msg: faker.music.songName(),
+        time: "10:01",
+        unread: 0,
+        pinned: false,
+      });
+    },
   },
 });
 
@@ -43,5 +87,17 @@ export default slice.reducer;
 export const FetchDirectConversations = ({ conversations }) => {
   return async (dispatch, getState) => {
     dispatch(slice.actions.fetchDirectConversations({ conversations }));
+  };
+};
+
+export const AddDirectConversation = ({ conversation }) => {
+  return async (dispatch, getState) => {
+    dispatch(slice.actions.addDirectConversation({ conversation }));
+  };
+};
+
+export const UpdateDirectConversation = ({ conversation }) => {
+  return async (dispatch, getState) => {
+    dispatch(slice.actions.updateDirectConversation({ conversation }));
   };
 };
